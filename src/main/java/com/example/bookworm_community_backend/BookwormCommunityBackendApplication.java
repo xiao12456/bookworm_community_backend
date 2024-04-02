da79cced3408809ef03e02467516728d9ef92360
wangwu <wangwu228056@gmail.com>
2024-04-02 14:37:35 +0800

    Initial commit

---
+++ src/main/java/com/example/bookworm_community_backend/BookwormCommunityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookworm_community_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwormCommunityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookwormCommunityBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookworm_community_backend/BookwormCommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookworm_community_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookwormCommunityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
